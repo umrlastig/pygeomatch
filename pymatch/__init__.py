c21191609896dc5b4d233285208efc6823e42e59
Julien Perret <julien.perret@gmail.com>
2026-01-16 11:54:06 +0100

    removed old file

--- pymatch/__init__.py
+++ pymatch/__init__.py
@@ -1,4 +1,4 @@
 from .multi_criteria import MCA, select_candidates
-from .geometric_matching_of_areas import surface_match, default_parameters
+from .geometric_matching_of_areas import surface_match
 
 from .multi_criteria2 import MCA2
